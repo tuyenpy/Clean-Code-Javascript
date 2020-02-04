9461a0af377336604c5abecc7e918d9c72550661
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-02-04 17:29:38 +0700

    Deep Nesting - example for Array

---
+++ deepNesting.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// |Input --- fn check array --> (true) --> fn check(call back) -- (true) --> ...........
+//                                (false) --> {console.log(item); return}                 
+
+const arr = [[[["value1"], "value2",[[["value3", [[[["value4", ["value5"]]]]]]]]]]];
+
+const checkArr = (arr) => {// fn check array
+  if (Array.isArray(arr)) {
+    arr.map( (item)=>{
+      if (Array.isArray(item)) {// true -> fn check - callback
+        checkArr(item)
+      } else { // false -> console.log(item); return;
+        console.log(item);
+        return;
+      }
+    } )
+  }
+}
+
+checkArr(arr);
+
+// Output expect:
+// value1
+// value2
+// value3
+// value4
+// value5
