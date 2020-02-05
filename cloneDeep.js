c78e0c246c4e345dbd89979ab578abcfc79da66d
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-02-05 15:19:51 +0700

    Best Clone Object

---
+++ cloneDeep.js
@@ -0,0 +1,20 @@
+"use strict";
+// clone Object by use JSON.parse() & JSON.stringify()
+
+
+const obj = {
+    a: 1,
+    b: {c: 1},
+    d: {e: 1, f: {g: 1}}
+}
+
+let text = JSON.stringify(obj); // object convert to string
+
+let cloneObject = JSON.parse(text); // string convert to object
+
+// console.log(obj);
+// console.log(cloneObject);
+
+console.log(cloneObject === obj);
+
+// Expect: false
--- cloneDeep_lodash.js
+++ cloneDeep_lodash.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-let obj = {a: 1, b: {c: 1, d: {e: 1}}};
+let obj = {a: 1, b: {c: 1, d: {e: 1}}}; // than 3 not work
 
 let copyObj = _.cloneDeep(obj);
 
