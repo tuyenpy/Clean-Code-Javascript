023174e6420da90af3a551d6a5fea4e2385919b9
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-03-12 11:23:42 +0700

    Delete file

--- cloneDeep_lodash.js
+++
@@ -1,9 +0,0 @@
-const _ = require('lodash');
-
-let obj = {a: 1, b: {c: 1, d: {e: 1}}}; // than 3 not work
-
-let copyObj = _.cloneDeep(obj);
-
-console.log(copyObj);
-
-console.log(copyObj === obj);
--- deepCloneArray.js
+++
@@ -1,8 +0,0 @@
-const arr1 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
-
-const convert = (arr) => JSON.parse(JSON.stringify(arr));
-
-const newArr = convert(arr1);
-
-console.log(newArr);
-
